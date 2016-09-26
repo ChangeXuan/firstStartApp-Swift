0223ca017bb1c30f1cb5a5282f8cdef2787236f2
李炜 <winxos@icloud.com>
2016-09-26 13:59:27 +0800

    Initial Commit

---
+++ firstStartApp/AppDelegate.swift
@@ -0,0 +1,74 @@
+//
+//  AppDelegate.swift
+//  firstStartApp
+//
+//  Created by www on 16/9/26.
+//  Copyright © 2016年 www. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        //得到当前的版本👌
+        let infoDictiomary = NSBundle.mainBundle().infoDictionary
+        let currentAppVersion = infoDictiomary!["CFBundleShortVersionString"] as! String
+        
+        //取出之前保存的版本号
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let appVersion = userDefaults.stringForKey("appVersion")
+        
+        let storyboard = UIStoryboard(name: "Main",bundle: nil)
+        
+        //appVersion ＝＝ nil说明是第一次启动；appVersion!=currntAppVersion说明是不等于当前的版本号，有更新
+        if appVersion == nil || appVersion != currentAppVersion {
+            userDefaults.setValue(currentAppVersion, forKey: "appVersion")
+            
+            let testView = storyboard.instantiateViewControllerWithIdentifier("ViewController1") as! ViewController1
+            self.window?.rootViewController = testView
+        }
+        
+//        let readData = 0
+//        
+//        if readData == 1 {
+//            let testView = storyboard.instantiateViewControllerWithIdentifier("ViewController2") as! ViewController2
+//            self.window?.rootViewController = testView
+//        } else {
+//            let testView = storyboard.instantiateViewControllerWithIdentifier("ViewController1") as! ViewController1
+//            self.window?.rootViewController = testView
+//        }
+//        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ firstStartApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  firstStartApp
+//
+//  Created by www on 16/9/26.
+//  Copyright © 2016年 www. All rights reserved.
+//代码在AppDelegate
+//注意SB的ID
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("two")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
+class ViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("one")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
